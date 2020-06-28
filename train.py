bdbfe872276d406954f8b39e47bc8ee59cc27765
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2020-06-29 00:05:38 +0700

    Add TesorBoard

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import logging
 import os
 
-from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCheckpoint, TensorBoard
 from tensorflow.keras.optimizers import SGD
 from tqdm import tqdm
 
@@ -38,15 +38,18 @@ def fit_model(dsm: DeepSpeakerModel, working_dir: str, max_length: int = NUM_FRA
 
     if classify:
         checkpoint_filename = os.path.join(CHECKPOINTS_CLASSIFY_DIR, checkpoint_name + '_{epoch}.h5')
+        logdir = "./logs/classify" + datetime.now().strftime("%Y%m%d-%H%M%S")
     else:
         checkpoint_filename = os.path.join(CHECKPOINTS_TRIPLET_DIR, checkpoint_name + '_{epoch}.h5')
+        logdir = "./logs/triplet" + datetime.now().strftime("%Y%m%d-%H%M%S")
 
     checkpoint = ModelCheckpoint(monitor='val_loss', filepath=checkpoint_filename, save_best_only=True)
     early_stopping = EarlyStopping(monitor='loss', min_delta=0.001, patience=20, verbose=1)
+    tensorboard_callback = TensorBoard(log_dir=logdir)
 
     dsm.m.fit_generator(train_generator(), steps_per_epoch=2000, shuffle=False,
               epochs=epochs, validation_data=test_generator(), validation_steps=len(test_batches),
-              initial_epoch=initial_epoch, callbacks=[checkpoint, early_stopping])
+              initial_epoch=initial_epoch, callbacks=[checkpoint, early_stopping, tensorboard_callback])
 
 
 def fit_model_softmax(dsm: DeepSpeakerModel, kx_train, ky_train, kx_test, ky_test,
@@ -60,6 +63,8 @@ def fit_model_softmax(dsm: DeepSpeakerModel, kx_train, ky_train, kx_test, ky_tes
 
     # if the accuracy does not increase over 10 epochs, we reduce the learning rate by half.
     reduce_lr = ReduceLROnPlateau(monitor='val_accuracy', factor=0.5, patience=10, min_lr=0.0001, verbose=1)
+    logdir = "./logs/softmax" + datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = TensorBoard(log_dir=logdir)
 
     max_len_train = len(kx_train) - len(kx_train) % batch_size
     kx_train = kx_train[0:max_len_train]
@@ -76,13 +81,17 @@ def fit_model_softmax(dsm: DeepSpeakerModel, kx_train, ky_train, kx_test, ky_tes
               verbose=1,
               shuffle=True,
               validation_data=(kx_test, ky_test),
-              callbacks=[early_stopping, reduce_lr, checkpoint])
+              callbacks=[early_stopping, reduce_lr, checkpoint, tensorboard_callback])
 
 
 def start_training(working_dir, pre_training_phase=True, epochs=1000, classify=False):
     ensures_dir(CHECKPOINTS_SOFTMAX_DIR)
     ensures_dir(CHECKPOINTS_TRIPLET_DIR)
     ensures_dir(CHECKPOINTS_CLASSIFY_DIR)
+    ensures_dir('./logs')
+    ensures_dir('./logs/softmax')
+    ensures_dir('./logs/triplet')
+    ensures_dir('./logs/classify')
     batch_input_shape = [None, NUM_FRAMES, NUM_FBANKS, 1]
     if pre_training_phase:
         logger.info('Softmax pre-training.')
